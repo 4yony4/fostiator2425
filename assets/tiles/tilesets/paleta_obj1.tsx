fd26fe0970a26d6917b5baeb44373ed254d257b0
Eurekamps <4yony4@gmail.com>
2025-01-10 13:27:56 +0100

    Incorporar tiles set y gravedad y movimiento al juego

---
+++ assets/tiles/tilesets/paleta_obj1.tsx
@@ -0,0 +1,43 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="paleta_obj1" tilewidth="282" tileheight="301" tilecount="13" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../../images/imgs_tiles/obj1/Bush (1).png" width="133" height="65"/>
+ </tile>
+ <tile id="1">
+  <image source="../../images/imgs_tiles/obj1/Bush (2).png" width="133" height="65"/>
+ </tile>
+ <tile id="2">
+  <image source="../../images/imgs_tiles/obj1/Bush (3).png" width="73" height="47"/>
+ </tile>
+ <tile id="3">
+  <image source="../../images/imgs_tiles/obj1/Bush (4).png" width="73" height="46"/>
+ </tile>
+ <tile id="4">
+  <image source="../../images/imgs_tiles/obj1/Crate.png" width="77" height="77"/>
+ </tile>
+ <tile id="5">
+  <image source="../../images/imgs_tiles/obj1/Mushroom_1.png" width="49" height="41"/>
+ </tile>
+ <tile id="6">
+  <image source="../../images/imgs_tiles/obj1/Mushroom_2.png" width="50" height="41"/>
+ </tile>
+ <tile id="7">
+  <image source="../../images/imgs_tiles/obj1/Sign_1.png" width="63" height="65"/>
+ </tile>
+ <tile id="8">
+  <image source="../../images/imgs_tiles/obj1/Sign_2.png" width="62" height="64"/>
+ </tile>
+ <tile id="9">
+  <image source="../../images/imgs_tiles/obj1/Stone.png" width="90" height="54"/>
+ </tile>
+ <tile id="10">
+  <image source="../../images/imgs_tiles/obj1/Tree_1.png" width="116" height="44"/>
+ </tile>
+ <tile id="11">
+  <image source="../../images/imgs_tiles/obj1/Tree_2.png" width="282" height="301"/>
+ </tile>
+ <tile id="12">
+  <image source="../../images/imgs_tiles/obj1/Tree_3.png" width="282" height="275"/>
+ </tile>
+</tileset>
